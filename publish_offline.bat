a361437a861ca1ba61e9106d210bf8e3ae41d552
Lasereyes5 <1769727743@qq.com>
2025-12-02 00:29:30 +0800

    Delete publish_offline.bat
    离线发布只能在已下载WinFormsComInterop和aot有关nuget包（发布过aot程序）时才能执行，否则需要包含约半个G的aot依赖的所有nuget包，太笨重了

--- publish_offline.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Lain
-
-cd KardsGen
-dotnet nuget disable source nuget.org
-dotnet publish /restore /p:RestoreAdditionalProjectSources=packages
-dotnet nuget enable source nuget.org
